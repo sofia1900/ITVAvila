225cfe61b590363e494a02790189f3fb453054f1
sofia1900 <115726194+sofia1900@users.noreply.github.com>
2023-01-10 17:55:00 +0100

    Uso de los casos de uso en main

--- app/src/main/java/com/iesam/itvavila/presentation/Main.java
+++ app/src/main/java/com/iesam/itvavila/presentation/Main.java
@@ -9,6 +9,12 @@ import com.iesam.itvavila.domain.models.Camion;
 import com.iesam.itvavila.domain.models.Coche;
 import com.iesam.itvavila.domain.models.Inspeccion;
 import com.iesam.itvavila.domain.models.Propietario;
+import com.iesam.itvavila.domain.usecase.GuardarInspeccionUseCase;
+import com.iesam.itvavila.domain.usecase.GuardarPropietarioUseCase;
+import com.iesam.itvavila.domain.usecase.GuardarVehiculoUseCase;
+import com.iesam.itvavila.domain.usecase.ObtenerInspeccionUseCase;
+import com.iesam.itvavila.domain.usecase.ObtenerPropietarioUseCase;
+import com.iesam.itvavila.domain.usecase.ObtenerVehiculoUseCase;
 
 public class Main extends AppCompatActivity {
 
@@ -27,6 +33,10 @@ public class Main extends AppCompatActivity {
         camion.setLon("10m");
         camion.setTara("3000kg");
 
+        //Guardo el camión como vehículo
+        GuardarVehiculoUseCase guardarVehiculoUseCase = new GuardarVehiculoUseCase();
+        guardarVehiculoUseCase.execute(camion);
+
         //Crear coche
         Coche coche = new Coche();
         coche.setMarca("Seat");
@@ -35,6 +45,9 @@ public class Main extends AppCompatActivity {
         coche.setAnnioMatriculacion("2022");
         coche.setMatricula("0000-GYU");
         coche.setNumPuertas(5);
+
+        //Guardo el coche como vehículo que es. Nota: No hace falta crear una nueva instancia del caso de uso.
+        guardarVehiculoUseCase.execute(coche);
 
         //Crear Propietario
         Propietario propietario = new Propietario();
@@ -47,6 +60,16 @@ public class Main extends AppCompatActivity {
         propietario.setProvincia("Ávila");
         propietario.setTelefono("700 000 000");
 
+        //Guardo con Caso de Uso (en memoria)
+        GuardarPropietarioUseCase guardarPropietarioUseCase = new GuardarPropietarioUseCase();
+        guardarPropietarioUseCase.execute(propietario);
+
+        //Creo un caso de uso para recuperar vehículos
+        ObtenerVehiculoUseCase obtenerVehiculoUseCase = new ObtenerVehiculoUseCase();
+
+        //Creo un caso de uso para recuperar propietario
+        ObtenerPropietarioUseCase obtenerPropietarioUseCase = new ObtenerPropietarioUseCase();
+
         //Crear inspección 1
         Inspeccion inspeccion = new Inspeccion();
         inspeccion.setCodigo("1");
@@ -55,6 +78,10 @@ public class Main extends AppCompatActivity {
         inspeccion.setPropietario(propietario);
         inspeccion.setVehiculo(coche);
 
+        //Guardo la inspección
+        GuardarInspeccionUseCase guardarInspeccionUseCase = new GuardarInspeccionUseCase();
+        guardarInspeccionUseCase.execute(inspeccion);
+
         //Crear inspección 2
         Inspeccion inspeccion2 = new Inspeccion();
         inspeccion2.setCodigo("2");
@@ -62,6 +89,12 @@ public class Main extends AppCompatActivity {
         inspeccion2.setResultado(false);
         inspeccion2.setPropietario(propietario);
         inspeccion2.setVehiculo(camion);
+
+        //Guardo la inspección.
+        guardarInspeccionUseCase.execute(inspeccion2);
+
+        //Creo caso de uso para recuperar inspecciones
+        ObtenerInspeccionUseCase obtenerInspeccionUseCase = new ObtenerInspeccionUseCase();
 
         InspeccionPrinter inspeccionPrinter = new InspeccionPrinter();
         //Imprimo la inspección 1
